29a80f32fcfe4ad99e67a3f4e41efe707a72bd72
jake dapper <jakedapper@gmail.com>
2023-02-20 12:06:13 -0600

    solution mileage

--- mileage.py
+++ mileage.py
@@ -1,10 +1,13 @@
 print("How many kilometers did you cycle today?")
-kms = input() #get user input
-miles = float(kms)/1.60934 #convert from string to float and divide
-miles = round(miles, 2) #round the result
+
+kms = input()
+
+miles = float(kms)/1.60934 
+
+miles = round(miles, 2) 
+
 print(f"Your {kms}km ride was {miles}mi ")  
 
-# ROUND SYNTAX:
-# round(thing to round, how many decimal points)
+
 
 
